c94e0e8fb4c48240343165e984d7f5026d3151d1
xuxuxu-123 <72958020+xusong-20@users.noreply.github.com>
2022-07-04 09:24:37 +0800

    Update __init__.py

--- model/network/__init__.py
+++ model/network/__init__.py
@@ -1,1 +1,5 @@
-
+from .triplet import TripletLoss
+from .crossentropy import CELoss
+from .distillation import LocalDistillLoss, GlobalDistillLoss, GlobalDistillLoss_Hard
+from .gaitset import SetNet
+from .lightgaitset import LightSetNet
